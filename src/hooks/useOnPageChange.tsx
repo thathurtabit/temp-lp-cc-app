6dd97108b2e66edd12e3b0822adf97cb9de78b7d
Stephen Fairbanks <stephen.fairbanks@autotrader.co.uk>
2021-09-06 14:21:28 +0100

    Update useOnPageChange.tsx

--- src/hooks/useOnPageChange.tsx
+++ src/hooks/useOnPageChange.tsx
@@ -3,11 +3,11 @@ import { useEffect } from "react";
 export const useOnPageChange = (URL: string) => {
   useEffect(() => {
     // Close active window
-    // console.log("LivePerson: attempt to close active window");
-    // window.lpTag?.taglets?.lpUnifiedWindow &&
-    //   window.lpTag.taglets.lpUnifiedWindow.onBeforeNavigation({
-    //     dispose: true,
-    //   });
+    console.log("LivePerson: attempt to close active window");
+    window.lpTag?.taglets?.lpUnifiedWindow &&
+      window.lpTag.taglets.lpUnifiedWindow.onBeforeNavigation({
+        dispose: true,
+      });
 
     console.log("LivePerson: window.lpTag.newPage(...)");
     // https://developers.liveperson.com/web-tag-new-page-refresh.html
@@ -23,5 +23,5 @@ export const useOnPageChange = (URL: string) => {
           },
         },
       });
-  });
+  }, [URL]);
 };
