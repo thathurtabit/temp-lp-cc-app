eaa819f0a6a0bb16168744464302b3910121069d
Gan <ganmel@gmail.com>
2021-09-08 12:56:40 +0100

    remove upstream/stashed

--- src/hooks/useOnPageChange.tsx
+++ src/hooks/useOnPageChange.tsx
@@ -3,13 +3,13 @@ import { useEffect } from "react";
 export const useOnPageChange = (URL: string) => {
   useEffect(() => {
     // Close active window
-<<<<<<< Updated upstream
+
     console.log("LivePerson: attempt to close active window");
     // window.lpTag?.taglets?.lpUnifiedWindow &&
     //   window.lpTag.taglets.lpUnifiedWindow.onBeforeNavigation({
     //     dispose: true,
     //   });
-=======
+
   // console.log("Test");
 
   //   console.log("LivePerson: attempt to close active window");
@@ -17,18 +17,14 @@ export const useOnPageChange = (URL: string) => {
   //     window.lpTag.taglets.lpUnifiedWindow.onBeforeNavigation({
   //       dispose: true,
   //     });
->>>>>>> Stashed changes
 
     console.log("LivePerson: window.lpTag.newPage(...)");
     // https://developers.liveperson.com/web-tag-new-page-refresh.html
     window?.lpTag?.newPage &&
-<<<<<<< Updated upstream
       window.lpTag.newPage(URL, {
         section: [],
         sdes: [],
       });
-=======
       window.lpTag.newPage(URL);
->>>>>>> Stashed changes
   }, [URL]);
 };
